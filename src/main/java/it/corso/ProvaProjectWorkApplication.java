4c12386751a329daf05ed50d039dda333a89daba
Viviana <vivianaman378@gmail.com>
2024-01-30 17:01:40 +0100

    initial commit + gitignore

---
+++ src/main/java/it/corso/ProvaProjectWorkApplication.java
@@ -0,0 +1,13 @@
+package it.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaProjectWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaProjectWorkApplication.class, args);
+	}
+
+}
